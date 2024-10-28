ed224c3022b5786d2c9bb4bb26f1b91a9d4e87e7
Federico Carranza <fcarranz@cbr6s1.42barcelona.com>
2024-10-28 11:04:31 +0100

    ex02 - Complete ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 10:46:58 by fcarranz          #+#    #+#             */
+/*   Updated: 2024/10/28 10:59:54 by fcarranz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int	main() {
+	std::string		ex02 = "HI THIS IS BRAIN";
+	std::string*	stringPTR = &ex02;
+	std::string&	stringREF = ex02;
+
+	std::cout << "The memory address of the string variable:	[" << &ex02 << "]" << std::endl;
+	std::cout << "The memory address held by stringPTR: 		[" << stringPTR << "]" << std::endl;
+	std::cout << "The memory address held by stringREF: 		[" << &stringREF << "]" << std::endl;
+
+	std::cout << "The value of the string variable: 		[" << ex02 << "]" << std::endl;
+	std::cout << "The value pointed to by stringPTR: 		[" << *stringPTR << "]" << std::endl;
+	std::cout << "The value pointed to by stringREF: 		[" << stringREF << "]" << std::endl;
+	
+	return 0;
+}
