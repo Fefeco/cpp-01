d9a71b1ea13e249cbe5822fec73317549bc2b12c
Federico Carranza <fcarranz@cbr6s1.42barcelona.com>
2024-11-01 21:32:39 +0100

    ex05 - Add newline to complain functions

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 21:25:06 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/11/01 19:05:35 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/11/01 21:30:45 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,23 +37,23 @@ void Harl::complain( std::string level) {
 void Harl::debug( void ) {
     std::cout << DEBUG
               << "⥱ I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!"
-              << RST << std::endl;
+              << RST << std::endl << std::endl;
 }
 
 void Harl::info ( void ) {
     std::cout << INFO
               << "🛈 I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!"
-              << RST << std::endl;
+              << RST << std::endl << std::endl;
 }
 
 void Harl::warning ( void ) {
     std::cout << WARNING
               << "⚠ I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month."
-              << RST << std::endl;
+              << RST << std::endl << std::endl;
 }
 
 void Harl::error ( void ) {
     std::cout << ERROR
               << "✕ This is unacceptable! I want to speak to the manager now."
-              << RST << std::endl;
+              << RST << std::endl << std::endl;
 }
--- ex05/main.cpp
+++ ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 17:31:25 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/11/01 18:58:37 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/11/01 21:31:02 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,19 @@ int main( void ) {
     Harl    harl;
 
     std::cout << "--- TESTING ---" << std::endl
-              << "\nHarl with warning call"<< std::endl;
+              << "Harl with warning call"<< std::endl;
     harl.complain("WARNING");
 
-    std::cout << "\nHarl with info call"<< std::endl;
+    std::cout << "Harl with info call"<< std::endl;
     harl.complain("INFO");
 
-    std::cout << "\nHarl with error call"<< std::endl;
+    std::cout << "Harl with error call"<< std::endl;
     harl.complain("ERROR");
 
-    std::cout << "\nHarl with debug call"<< std::endl;
+    std::cout << "Harl with debug call"<< std::endl;
     harl.complain("DEBUG");
 
-    std::cout << "\nHarl with other call"<< std::endl;
+    std::cout << "Harl with other call"<< std::endl;
     harl.complain("OTHER");
 
     std::cout << "\n--- END ---" << std::endl;
