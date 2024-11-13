304c6a41c7769c052122a5e0831e00ec4bea0a3a
Federico Carranza <fcarranz@cbr6s1.42barcelona.com>
2024-11-13 11:45:34 +0100

    ex01 - FIX BUG: add checks for negative value and empty string in zombieHorde()

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 16:32:12 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/10/28 10:41:37 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/11/13 11:36:42 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,14 @@ int main( void ) {
 	int		N = 11;
 
 	horde = zombieHorde( N, "Norm" );
+	if ( !horde )
+		return 1;
 	
-	for (int i = 0; i < N; ++i) {
+	for (int i = 0; i < N; ++i)
 		horde[i].announce();
-	}
 
-	for (int i = 0; i < N; ++i) {
+	for (int i = 0; i < N; ++i)
 		horde[i].~Zombie();
-	}
 	
 	operator delete[]( horde );
 	return 0;
--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -6,13 +6,16 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 13:54:31 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/10/28 10:37:18 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/11/13 11:31:36 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 Zombie* zombieHorde( int N, std::string name ) {
+	if ( N <= 0 || name.empty() )
+		return NULL;
+
 	Zombie* horde = static_cast<Zombie*>( operator new[]( sizeof( Zombie ) * N ) );
 	
 	for ( int i = 0; i < N; ++i ) {
